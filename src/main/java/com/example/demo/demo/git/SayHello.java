6af4c7d9b49eeb16f278933881b796fa531a390b
Forest <mistraf16@yahoo.fr>
2018-04-05 23:54:10 +0200

    Forest committed

--- src/main/java/com/example/demo/demo/git/SayHello.java
+++ src/main/java/com/example/demo/demo/git/SayHello.java
@@ -1,7 +1,9 @@
-package com.example.demo;
+package com.example.demo.demo.git;
 
 public class SayHello {
     public static void main(String[]args){
         System.out.println("Hello World");
+        EvenNumbers.printEvenNumber(11);
+        OddNumbers.printOddNumber(12);
     }
 }
